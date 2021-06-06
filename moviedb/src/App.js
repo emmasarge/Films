17be446b8ab65dc2bb83b212a23656079d3e5647
emmasarge <emmasargeant206@gmail.com>
2021-06-06 14:57:25 +0000

    add search bar

--- moviedb/src/App.js
+++ moviedb/src/App.js
@@ -3,12 +3,15 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import FilmList from './FilmList';
+import FilmTop from './FilmTop';
+import SearchBar from './SearchBar';
 
 const App = () => {
     const [movies, setMovies] = useState([]);
+    const [searchInput, setSearchInput]= useState('');
 
-        const fetchFilms = async () => {
-		const url = `https://www.omdbapi.com/?s=avengers&apikey=9431dc8`;
+        const fetchFilms = async (searchInput) => {
+		const url = `https://www.omdbapi.com/?s=${searchInput}&apikey=9431dc8`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
@@ -19,12 +22,17 @@ const App = () => {
 	};
 
     useEffect(() => {
-		fetchFilms();
-	}, []);
+		fetchFilms(searchInput);
+	}, [searchInput]);
 
 
     return (
     <div className='container-fluid moviedb'>
+       <div className='row d-flex align-items-center mt-4 mb-4'>
+           <FilmTop heading='FilmBase' />
+           <SearchBar searchInput={searchInput} setSearchInput={setSearchInput} /> 
+       </div>
+
         <div className='row'>
             <FilmList movies={movies}/>
         </div>
---
+++ moviedb/src/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchBar = (props) => {
+    return(
+        <div className='col col-sm-6'>
+            <input className='form-control searchbar' value={props.value}
+            onChange={(event)=> props.setSearchInput(event.target.value)} placeholder='Search for a film...'></input>
+
+        </div>
+    )
+};
+
+export default SearchBar;
